619879c28c5d00006b1d8d8500920a3a68a10734
Protus bantan <protus.bantan@gmail.com.com>
2018-09-08 17:04:08 +0300

    imported and defined usermix in models.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,37 @@
 from flask import render_template
 from . import main
+from flask_login import login_required,UserMixin
 
 # Views
 @main.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = 'Home - Welcome to The best Pitching Website Online'
 
+    search_pitch = request.args.get('pitch_query')
+    pitches= Pitch.get_all_pitches()  
+
+    return render_template('index.html', title = title, pitches= pitches)
+@main.route('/pitch/new/', methods = ['GET','POST'])
+@login_required
+def new_pitch():
+    '''
+    Function that creates new pitches
+    '''
+    form = PitchForm()
+
+
+    if category is None:
+        abort( 404 )
+
+    if form.validate_on_submit():
+        pitch= form.content.data
+        category_id = form.category_id.data
+        new_pitch= Pitch(pitch= pitch, category_id= category_id)
+
+        new_pitch.save_pitch()
+        return redirect(url_for('main.index'))
+
+    return render_template('new_pitch.html', new_pitch_form= form, category= category)
