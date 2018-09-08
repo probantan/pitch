329136c95aa037394b5b87487b50cc3645391c83
Protus bantan <protus.bantan@gmail.com.com>
2018-09-08 12:46:22 +0300

    imported Flask werseug security to to provide hashing functionality to you passwords stored

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class User(db.Model):
@@ -6,6 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
