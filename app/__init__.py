b838601587f1a411863b1e54df120004544c532b
Protus bantan <protus.bantan@gmail.com.com>
2018-09-08 14:59:35 +0300

    imported login manager

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,15 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
-from flask_login import LoginManager
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 # Creating app instance
 def create_app(config_name):
     app = Flask(__name__)
@@ -25,6 +27,6 @@ def create_app(config_name):
 # Initializing Flask Extensions
     bootstrap.init_app(app)
     db.init_app(app)
-
+    login_manager.init_app(app)
 
     return app
