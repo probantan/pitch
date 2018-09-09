47c97c541287f287c657d09c1dd4f5046c170866
Protus bantan <protus.bantan@gmail.com.com>
2018-09-09 10:05:39 +0300

    installed and imoprted flask email in main __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
--- config.py
+++ config.py
@@ -2,10 +2,22 @@ import os
 
 class Config:
   
-      SECRET_KEY = os.environ.get('SECRET_KEY')
-      SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://protus:suzy2015@localhost/pitch'
-      SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://protus:suzy2015@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+       #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'One Minute Pitch'
+    SENDER_EMAIL = 'protus.bantan@gmail.com'
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 class ProdConfig(Config):
     pass
