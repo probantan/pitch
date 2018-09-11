1ba7bc95d710e6fe1960b0f055eaf1e95556df89
Protus bantan <protus.bantan@gmail.com.com>
2018-09-11 16:35:32 +0300

    fixes manage and config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
   
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://protus:suzy2015@localhost/pitch'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://protus:suzy2015@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
@@ -23,6 +23,7 @@ class Config:
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://protus:suzy2015@localhost/pitch'
     DEBUG = True
 
 config_options = {
