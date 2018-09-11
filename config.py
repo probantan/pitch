cae1ad6f69493c5edfa85516e99af756fc66bef8
Protus bantan <protus.bantan@gmail.com.com>
2018-09-11 14:17:29 +0300

    initial

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
   
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://protus:suzy2015@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://protus:suzy2015@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User,Role
 from  flask_migrate import Migrate, MigrateCommand
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
